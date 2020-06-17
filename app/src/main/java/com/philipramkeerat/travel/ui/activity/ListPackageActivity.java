ae597383f38fe16c8dd57a66e4402b9c4b143396
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2020-06-17 02:12:50 -0400

    Import R class dynamic on ListPackageActivity

--- app/src/main/java/com/philipramkeerat/travel/ui/activity/ListPackageActivity.java
+++ app/src/main/java/com/philipramkeerat/travel/ui/activity/ListPackageActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.philipramkeerat.travel.R;
+
 public class ListPackageActivity extends AppCompatActivity {
 
     @Override
