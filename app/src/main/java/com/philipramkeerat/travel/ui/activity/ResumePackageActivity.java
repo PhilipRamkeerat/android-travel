a8d561be119be0a0a9cbac92aa1c59284e0c3017
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2020-06-21 12:23:07 -0400

    Init layout resume package and activity

---
+++ app/src/main/java/com/philipramkeerat/travel/ui/activity/ResumePackageActivity.java
@@ -0,0 +1,16 @@
+package com.philipramkeerat.travel.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.philipramkeerat.travel.R;
+
+public class ResumePackageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume_package);
+    }
+}
