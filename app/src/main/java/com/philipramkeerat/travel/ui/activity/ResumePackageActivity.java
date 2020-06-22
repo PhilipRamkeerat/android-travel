04972564a5bf3cdd2996be39fe45fa7a181d6570
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2020-06-21 22:27:05 -0400

    Implement dynamic date and adjust views date and prince to chain layout

--- app/src/main/java/com/philipramkeerat/travel/ui/activity/ResumePackageActivity.java
+++ app/src/main/java/com/philipramkeerat/travel/ui/activity/ResumePackageActivity.java
@@ -15,6 +15,8 @@ import com.philipramkeerat.travel.util.DaysUtil;
 import com.philipramkeerat.travel.util.ResourceUtil;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ResumePackageActivity extends AppCompatActivity {
 
@@ -22,6 +24,8 @@ public class ResumePackageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_package);
+
+        setTitle("Resumo do pacote");
 
         Package packageSaoPaulo = new Package("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
@@ -41,6 +45,18 @@ public class ResumePackageActivity extends AppCompatActivity {
         String brazilianCoin = CoinUtil.formatForBrazilian(packageSaoPaulo.getPrice());
         price.setText(brazilianCoin);
 
+        // Data calc
+        TextView date = findViewById(R.id.resume_package_data);
+        Calendar startDate = Calendar.getInstance();
+        Calendar backDate = Calendar.getInstance();
+        backDate.add(Calendar.DATE, packageSaoPaulo.getDays());
+        SimpleDateFormat brazilianFormat = new SimpleDateFormat("dd/MM");
+        String formatedStartDate = brazilianFormat.format(startDate.getTime());
+        String formatedBackDate = brazilianFormat.format(backDate.getTime());
+        String travelDateFormated = formatedStartDate + " - "
+                + formatedBackDate + " de "
+                + backDate.get(Calendar.YEAR);
+        date.setText(travelDateFormated);
 
     }
 }
