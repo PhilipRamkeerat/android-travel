9a7b4a7f236b949d74cef70b032b50f889bf7732
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2020-06-22 11:50:35 -0400

    add strings xml for congrats purchase

--- app/src/main/java/com/philipramkeerat/travel/ui/activity/ResumePurchaseActivity.java
+++ app/src/main/java/com/philipramkeerat/travel/ui/activity/ResumePurchaseActivity.java
@@ -17,28 +17,46 @@ import java.math.BigDecimal;
 
 public class ResumePurchaseActivity extends AppCompatActivity {
 
+    public static final String TITLE_APPBAR = "Resumo da compra";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_purchase);
 
-        setTitle("Resumo da compra");
+        setTitle(TITLE_APPBAR);
 
         Package packageSaoPaulo = new Package("São Paulo", "sao_paulo_sp", 2, new BigDecimal("255.99"));
 
-        TextView local = findViewById(R.id.resume_purchase_local_package);
-        local.setText(packageSaoPaulo.getLocal());
+        showLocal(packageSaoPaulo);
 
-        ImageView image = findViewById(R.id.resume_purchase_image_package);
-        Drawable drawablePackage = ResourceUtil.returnDrawable(this, packageSaoPaulo.getImage());
-        image.setImageDrawable(drawablePackage);
+        showImage(packageSaoPaulo);
 
-        TextView data = findViewById(R.id.resume_purchase_date);
-        String periodInText = DateUtil.periodToText(packageSaoPaulo.getDays());
-        data.setText(periodInText);
+        showData(packageSaoPaulo);
 
+        showPrice(packageSaoPaulo);
+    }
+
+    private void showPrice(Package packageName) {
         TextView price = findViewById(R.id.resume_purchase_price);
-        String brazilianCoin = CoinUtil.formatForBrazilian(packageSaoPaulo.getPrice());
+        String brazilianCoin = CoinUtil.formatForBrazilian(packageName.getPrice());
         price.setText(brazilianCoin);
+    }
+
+    private void showData(Package packageName) {
+        TextView data = findViewById(R.id.resume_purchase_date);
+        String periodInText = DateUtil.periodToText(packageName.getDays());
+        data.setText(periodInText);
+    }
+
+    private void showImage(Package packageName) {
+        ImageView image = findViewById(R.id.resume_purchase_image_package);
+        Drawable drawablePackage = ResourceUtil.returnDrawable(this, packageName.getImage());
+        image.setImageDrawable(drawablePackage);
+    }
+
+    private void showLocal(Package packageName) {
+        TextView local = findViewById(R.id.resume_purchase_local_package);
+        local.setText(packageName.getLocal());
     }
 }
