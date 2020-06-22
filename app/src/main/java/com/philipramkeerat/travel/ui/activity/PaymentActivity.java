61d20950b16a1cdd47b9896fc5c547310afeaaba
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2020-06-22 01:53:30 -0400

    Implement bind on payment activity for price

--- app/src/main/java/com/philipramkeerat/travel/ui/activity/PaymentActivity.java
+++ app/src/main/java/com/philipramkeerat/travel/ui/activity/PaymentActivity.java
@@ -3,14 +3,38 @@ package com.philipramkeerat.travel.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.philipramkeerat.travel.R;
+import com.philipramkeerat.travel.model.Package;
+import com.philipramkeerat.travel.util.CoinUtil;
+
+import java.math.BigDecimal;
 
 public class PaymentActivity extends AppCompatActivity {
+
+    public static final String TITLE_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        setTitle(TITLE_APPBAR);
+
+        Package packageSaoPaulo = new Package("São Paulo", "sao_paulo_sp", 2,
+                new BigDecimal("243.99"));
+
+        // Price bind
+        ShowPrice(packageSaoPaulo);
+
+
+    }
+
+    private void ShowPrice(Package packageName) {
+        TextView price = findViewById(R.id.payment_price_package);
+        String brazilianCoin = CoinUtil
+                .formatForBrazilian(packageName.getPrice());
+        price.setText(brazilianCoin);
     }
 }
